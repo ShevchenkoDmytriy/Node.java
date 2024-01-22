1af65db993fea9a68ea74f2dabcf55261902f3bb
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2024-01-22 10:33:27 +0200

    java

---
+++ untitled1/src/Main.java
@@ -0,0 +1,109 @@
+class CustomLinkedList {
+
+    private Node head; // голова списка
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void remove(int data) {
+        if (head == null) return;
+
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public int get(int index) {
+        Node current = head;
+        int count = 0;
+        while (current != null) {
+            if (count == index) return current.data;
+            count++;
+            current = current.next;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    public int findMiddle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.data;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        CustomLinkedList list = new CustomLinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println("Original List: ");
+        list.printList();
+
+        System.out.println("Reversed List: ");
+        list.reverse();
+        list.printList();
+
+        System.out.println("Middle Element: " + list.findMiddle());
+    }
+}
